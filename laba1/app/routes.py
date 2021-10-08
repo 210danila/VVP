6eb5e98ee86cd414538b1aef4d9d1e2d76a89e79
210danila <210danila@gmail.com>
2021-10-08 16:49:46 +0300

    first commit

---
+++ laba1/app/routes.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+	user = {'username':'student'}
+	posts = [
+ 		{
+			'author': {'username': 'John'},
+			'body': 'Beautiful day in Portland!'
+		},
+		{
+			'author': {'username': 'Susan'},
+			'body': 'The Avengers movie was so cool!'
+		},
+		{
+			'author': {'username': 'vasya'},
+			'body':'hi!!'
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
